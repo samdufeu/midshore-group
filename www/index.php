6618c47b43dc8facc7ee7dbec3560cd87e04416a
samdufeu <33517234+samdufeu@users.noreply.github.com>
2023-09-28 09:23:46 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Coming Soon</title>
+  <meta name="description" content="Midshore Group is a group of companies headquartered in Guernsey, Channel Islands. Call us on +44 (0) 1481 730733.">
+  <title>Midshore Group | Consulting | Compliance Services | Training </title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
   <style>
     body {
