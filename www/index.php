1057230093a156ce03886de9b96bf6c7459f6f1e
samdufeu <33517234+samdufeu@users.noreply.github.com>
2023-09-28 09:14:30 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,7 @@
       font-family: Arial, sans-serif;
       padding: 20px;
     }
+
     .container {
       text-align: center;
       margin-top: 20px;
@@ -19,6 +21,7 @@
       justify-content: center;
       align-items: center;
     }
+
     .column {
       padding: 20px;
       height: 33vh;
@@ -26,6 +29,7 @@
       border: 1px solid #ddd;
       box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     }
+
     .column::before {
       content: '';
       position: absolute;
@@ -35,12 +39,14 @@
       width: 100%;
       background: linear-gradient(to left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.1));
     }
+
     .column img {
       max-width: 100%;
       height: auto;
       display: block;
       margin: 0 auto;
     }
+
     .overlay-text {
       font-size: 14px;
       color: #fff;
@@ -48,61 +54,70 @@
       padding: 5px 10px;
       border-radius: 5px;
     }
+
     .card {
       border: none;
     }
+
     .card-footer {
       background-color: inherit;
       border: none;
       padding: 0;
     }
+
     .card-footer .overlay-text {
       display: block;
       margin-top: 10px;
     }
+
     @media (max-width: 767px) {
-      .column img { max-width: 375px; }
+      .column img {
+        max-width: 375px;
+      }
+
       .column {
         height: 33%;
         margin-bottom: 10px;
       }
+
       .container {
         height: auto;
       }
     }
   </style>
 </head>
+
 <body>
   <div class="container">
     <div class="row">
       <div class="col-md-4">
         <div class="card column">
           <a href="https://www.midshoreconsulting.com/" target="_blank">
-            <img src="https://www.midshoreconsulting.com/wp-content/uploads/2020/12/midshore_consulting_logo_377x115.png" alt="Visit Midshore Consulting Lim" class="card-img-top">
+            <img src="https://www.midshoreconsulting.com/wp-content/uploads/2020/12/midshore_consulting_logo_377x115.png" alt="Visit Midshore Consulting Limited" class="card-img-top">
+            <div class="card-footer">
+              <div class="overlay-text">Visit Midshore Consulting Limited</div>
+            </div>
           </a>
-          <div class="card-footer">
-            <div class="overlay-text">Visit Midshore Consulting Lim</div>
-          </div>
         </div>
       </div>
       <div class="col-md-4">
         <div class="card column">
           <a href="https://www.midshoreonlinetraining.com/" target="_blank">
             <img src="https://www.midshoreconsulting.com/wp-content/uploads/2023/09/midshore_online_training.png" alt="Visit Midshore Online Training" class="card-img-top">
+            <div class="card-footer">
+              <div class="overlay-text">Visit Midshore Online Training</div>
+            </div>
           </a>
-          <div class="card-footer">
-            <div class="overlay-text">Visit Midshore Online Training</div>
-          </div>
         </div>
       </div>
       <div class="col-md-4">
         <div class="card column disabled">
           <a href="#" target="_blank" class="disabled">
             <img src="https://www.midshoreconsulting.com/wp-content/uploads/2023/09/midshore-commpliance-services.png" alt="Midshore Compliance Services" class="card-img-top">
+            <div class="card-footer">
+              <div class="overlay-text">Coming Soon</div>
+            </div>
           </a>
-          <div class="card-footer">
-            <div class="overlay-text">Coming Soon</div>
-          </div>
         </div>
       </div>
     </div>
@@ -110,4 +125,5 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
